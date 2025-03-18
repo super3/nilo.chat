0fcb9c915c84c47f5f54bdbf6f8f4dae5def361e
Shawn Wilkinson <me@super3.org>
2025-03-18 14:20:19 -0400

    Fix Warning

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack')
+
 module.exports = {
   publicPath: './',
   outputDir: 'dist',
@@ -6,6 +8,13 @@ module.exports = {
     output: {
       filename: 'js/[name].[contenthash:8].js',
       chunkFilename: 'js/[name].[contenthash:8].js'
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
+      })
+    ]
   }
 } 
