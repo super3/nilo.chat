dd80f37fa395b13eb8608998d8575d63f652822a
Shawn Wilkinson <me@super3.org>
2025-03-18 12:54:40 -0400

    Try Again

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,15 @@ set -e
 echo "Building application..."
 npm run build
 
+# create 404.html
+echo "Creating 404.html for SPA routing..."
+cp dist/index.html dist/404.html
+
 # navigate into the build output directory
 cd dist
 
 # if you are deploying to a custom domain
+echo "Adding CNAME..."
 echo 'nilo.chat' > CNAME
 
 echo "Preparing for git deployment..."
@@ -18,8 +23,19 @@ git init
 git add -A
 git commit -m 'Deploy to GitHub Pages'
 
+# get the repository URL
+REPO_URL=$(git config --get remote.origin.url)
+echo "Repository URL: $REPO_URL"
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:$(git config --get remote.origin.url | cut -d: -f2 | cut -d. -f1) master:gh-pages
+echo "Pushing to gh-pages branch..."
+if [[ $REPO_URL == *"github.com"* ]]; then
+  # For GitHub repositories
+  git push -f $REPO_URL master:gh-pages
+else
+  echo "Error: Not a GitHub repository"
+  exit 1
+fi
 
 cd -
 
