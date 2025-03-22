a655c4084b4309250789481897cb546b5feaeebd
Shawn Wilkinson <me@super3.org>
2025-03-21 20:04:46 -0400

    Update ChatContent.vue

--- src/components/ChatContent.vue
+++ src/components/ChatContent.vue
@@ -81,7 +81,7 @@ export default {
     // Connect to the WebSocket server
     const socketUrl = isLocalhost 
       ? 'http://localhost:3000' 
-      : 'https://api.nilo.chat:3000';
+      : 'https://api.nilo.chat';
     
     console.log(`Connecting to Socket.IO server at: ${socketUrl}`);
     this.socket = io(socketUrl);
