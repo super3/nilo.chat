f55018b3abdeb8624ff3ce2887921bd6758cba9a
Shawn Wilkinson <me@super3.org>
2025-03-27 11:41:50 -0400

    Update DirectMessageSidebar.vue
    Removed Unused Bell Icon

--- src/components/DirectMessageSidebar.vue
+++ src/components/DirectMessageSidebar.vue
@@ -8,11 +8,6 @@
           <span class="text-white/50 text-sm">{{ username }}</span>
         </div>
       </div>
-      <div>
-        <svg class="h-6 w-6 fill-current text-white/25" viewBox="0 0 20 20">
-          <path d="M14 8a4 4 0 1 0-8 0v7h8V8zM8.027 2.332A6.003 6.003 0 0 0 4 8v6l-3 2v1h18v-1l-3-2V8a6.003 6.003 0 0 0-4.027-5.668 2 2 0 1 0-3.945 0zM12 18a2 2 0 1 1-4 0h4z" fill-rule="evenodd" />
-        </svg>
-      </div>
     </div>
     <div class="mb-8">
       <div class="px-4 mb-2 text-white flex justify-between items-center">
